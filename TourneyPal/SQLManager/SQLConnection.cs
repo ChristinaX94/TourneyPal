65af118ed511f98847eb934f76c40de7e4828d6a
Christina <christina.trial94@gmail.com>
2024-03-24 20:26:05 +0200

    "1.0.0.2" -> Changes to Versioning - Changes to Connection configurations - SQL models additions - Additions of Logger/Result/General

---
+++ TourneyPal/SQLManager/SQLConnection.cs
@@ -0,0 +1,214 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourneyPal.Commons;
+using TourneyPal.SQLManager;
+using TourneyPal.SQLManager.DataModels;
+using static Mysqlx.Expect.Open.Types.Condition.Types;
+
+namespace TourneyPal.SQLManager
+{
+    public class SQLConnection
+    {
+        private string server;
+        private string database;
+        private string username;
+        private string password;
+
+        private MySqlConnection connection;
+
+        public SQLConnection()
+        {
+            server = System.Configuration.ConfigurationManager.AppSettings["server"];
+            database = System.Configuration.ConfigurationManager.AppSettings["database"];
+            username = System.Configuration.ConfigurationManager.AppSettings["username"];
+            password = System.Configuration.ConfigurationManager.AppSettings["password"];
+        }
+
+        public Result cycleAppend(SQLItem sql)
+        {
+            Result result = new Result();
+            try
+            {
+                result = connect();
+                if (!result.success)
+                {
+                    return result;
+                }
+
+                result = executeQuery(sql);
+                if (!result.success)
+                {
+                    return result;
+                }
+
+                result = disconnect();
+                if (!result.success)
+                {
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        public Result cycleRead(SQLItem sql, Model model)
+        {
+            Result result = new Result();
+            try
+            {
+                result = connect();
+                if (!result.success)
+                {
+                    result.message = "Error";
+                    return result;
+                }
+
+                var obj = executeReadQuery(sql, model);
+                if (obj == null)
+                {
+                    result.message = "Error";
+                    return result;
+                }
+
+                result = disconnect();
+                if (!result.success)
+                {
+                    result.message = "Error";
+                    return result;
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        private Result connect()
+        {
+            Result result = new Result();
+            try
+            {
+                string connectionString = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + username + ";password=" + password;
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                result.success = true;
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        private Result executeQuery(SQLItem sql)
+        {
+            Result result = new Result();
+            try
+            {
+                MySqlCommand cmd = getMySqlCommand(sql);
+                if (cmd == null)
+                {
+                    result.success = false;
+                    return result;
+                }
+
+                cmd.ExecuteNonQuery();
+
+                result.success = true;
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        private Result executeReadQuery(SQLItem sql, Model model)
+        {
+            Result result = new Result();
+            try
+            {
+                MySqlCommand cmd = getMySqlCommand(sql);
+                if (cmd == null)
+                {
+                    result.success = false;
+                    return result;
+                }
+
+                cmd.ExecuteNonQuery();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                result = model.load(reader);
+                if (!result.success)
+                {
+                    return result;
+                }
+
+                result.success = true;
+
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        private MySqlCommand getMySqlCommand(SQLItem sql)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql.query, connection);
+
+                if (sql.parameters != null && sql.parameters.Count > 0)
+                {
+                    foreach (var parameter in sql.parameters)
+                    {
+                        cmd.Parameters.Add(parameter);
+                    }
+                }
+
+                return cmd;
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        private Result disconnect()
+        {
+            Result result = new Result();
+            try
+            {
+                connection.Close();
+                result.success = true;
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+    }
+
+
+
+}
+
