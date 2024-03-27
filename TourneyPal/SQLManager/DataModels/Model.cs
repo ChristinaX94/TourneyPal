61aea9e5314a1f8bf4e5857f47e153d89aa8078c
Christina <christina.trial94@gmail.com>
2024-03-27 22:27:16 +0200

    ("0.0.0.3") -> Fixes in versioning and completion of save SQL methods

--- TourneyPal/SQLManager/DataModels/Model.cs
+++ TourneyPal/SQLManager/DataModels/Model.cs
@@ -5,130 +5,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TourneyPal.Commons;
+using static TourneyPal.SQLManager.DataModels.SQLTables.Game_On_Tournament_Host_Site.Game_On_Tournament_Host_Site;
 using Result = TourneyPal.Commons.Result;
 
 namespace TourneyPal.SQLManager.DataModels
 {
     public abstract class Model : iModel
     {
-        public class ModelRow
+        public List<ModelRow> rows { get; private set; }
+
+        public Model()
         {
-            public ModelRow(string? tableName)
-            {
-                this.tableName = tableName;
-            }
-
-            private string? tableName { get; set; }
-            public int ID { get; private set; }
-            public DateTime DateUpdated { get; private set; }
-            public DateTime DateInserted { get; private set; }
-
-            protected Result loadRow(MySqlDataReader reader)
-            {
-                Result result = new Result();
-                try
-                {
-                    //ID
-                    var id = convertToInt(nameof(this.ID), reader[nameof(this.ID)]?.ToString());
-                    if (id == null)
-                    {
-                        result.success = false;
-                        return result;
-                    }
-                    this.ID =  (int)id;
-
-                    //DateUpdated
-                    var dateUpdated = convertToDateTime(nameof(this.DateUpdated), reader[nameof(this.DateUpdated)]?.ToString());
-                    if (dateUpdated == null)
-                    {
-                        result.success = false;
-                        return result;
-                    }
-                    this.DateUpdated = (DateTime)dateUpdated;
-
-                    //DateInserted
-                    var dateInserted = convertToDateTime(nameof(this.DateInserted), reader[nameof(this.DateInserted)]?.ToString());
-                    if (dateInserted == null)
-                    {
-                        result.success = false;
-                        return result;
-                    }
-                    this.DateInserted = (DateTime)dateInserted;
-                }
-                catch (Exception ex)
-                {
-                    result.success = false;
-                    result.message = ex.Message;
-                }
-                return result;
-            }
-
-            protected int? convertToInt(string? name, string? value)
-            {
-                try
-                {
-                    Int32.TryParse(value, out var intValue);
-                    return intValue;
-                }
-                catch (Exception ex)
-                {
-                    Logger.log(messageItem: "Error converting " + name + ": " + value + ", of table: " + this.tableName, 
-                               exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-                    return null;
-                }
-            }
-
-            protected DateTime? convertToDateTime(string? name, string? value)
-            {
-                try
-                {
-                    DateTime.TryParse(value, out var dateValue);
-                    return dateValue;
-                }
-                catch (Exception ex)
-                {
-                    Logger.log(messageItem: "Error converting " + name + ": " + value + ", of table: " + this.tableName,
-                               exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-                    return null;
-                }
-            }
-
-            protected bool? convertToBool(string? name, string? value)
-            {
-                try
-                {
-                    bool.TryParse(value, out var boolValue);
-                    return boolValue;
-                }
-                catch (Exception ex)
-                {
-                    Logger.log(messageItem: "Error converting " + name + ": " + value + ", of table: " + this.tableName,
-                               exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-                    return null;
-                }
-            }
-
-            protected decimal? convertToDecimal(string? name, string? value)
-            {
-                try
-                {
-                    Decimal.TryParse(value, out var decimalValue);
-                    return decimalValue;
-                }
-                catch (Exception ex)
-                {
-                    Logger.log(messageItem: "Error converting " + name + ": " + value + ", of table: " + this.tableName,
-                               exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-                    return null;
-                }
-            }
+            this.rows = new List<ModelRow>();
         }
 
         public abstract Result load(MySqlDataReader reader);
 
         public abstract Result delete(MySqlDataReader reader);
 
-        public abstract Result save(MySqlDataReader reader);
+        public virtual Result save(MySqlDataReader reader)
+        {
+            Result result = new Result();
+            try
+            {
+                if (this.rows == null ||
+                   this.rows.Count == 0)
+                {
+                    result.success = true;
+                    result.message = "Nothing to save on " + GetType().Name;
+                    return result;
+                }
+
+                foreach (var row in rows)
+                {
+                    result = row.ID == 0 ? row.insertNewRowData() : row.insertRowData();
+                    if (!result.success)
+                    {
+                        result.success = false;
+                        return result;
+                    }
+
+                    result = row.validateRow();
+                    if (!result.success)
+                    {
+                        result.success = false;
+                        return result;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
 
     }
 }
---
+++ TourneyPal/SQLManager/DataModels/SQLTables/Tournament_Api_Data/Tournament_Api_DataRow.cs
@@ -0,0 +1,128 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourneyPal.Commons;
+
+namespace TourneyPal.SQLManager.DataModels.SQLTables.Tournament_Api_Data
+{
+    public class Tournament_Api_DataRow : ModelRow
+    {
+        public Tournament_Api_DataRow(string? tableName) : base(tableName)
+        {
+        }
+
+        public int? Tournament_ID { get; private set; }
+        public int? HostSite_ID { get; private set; }
+        public int? TournamentHostSite_ID { get; private set; }
+        public string? Request { get; private set; }
+        public string? Response { get; private set; }
+
+        public override Result loadRow(MySqlDataReader reader)
+        {
+            Result result = new Result();
+            try
+            {
+                result = base.loadRow(reader);
+                if (!result.success)
+                {
+                    return result;
+                }
+
+                //Tournament_ID
+                var tournament_ID = convertToInt(nameof(Tournament_ID), reader[nameof(Tournament_ID)]?.ToString());
+                if (tournament_ID == null)
+                {
+                    result.success = false;
+                    return result;
+                }
+                Tournament_ID = (int)tournament_ID;
+
+                //HostSite_ID
+                var hostSite_ID = convertToInt(nameof(HostSite_ID), reader[nameof(HostSite_ID)]?.ToString());
+                if (hostSite_ID == null)
+                {
+                    result.success = false;
+                    return result;
+                }
+                HostSite_ID = (int)hostSite_ID;
+
+                //TournamentHostSite_ID
+                var tournamentHostSite_ID = convertToInt(nameof(TournamentHostSite_ID), reader[nameof(TournamentHostSite_ID)]?.ToString());
+                if (tournamentHostSite_ID == null)
+                {
+                    result.success = false;
+                    return result;
+                }
+                TournamentHostSite_ID = (int)tournamentHostSite_ID;
+
+                //Request
+                if (reader[nameof(Request)] == null ||
+                    string.IsNullOrEmpty(reader[nameof(Request)].ToString()))
+                {
+                    result.success = false;
+                    return result;
+                }
+                Request = reader[nameof(Request)].ToString();
+
+                //Response
+                if (reader[nameof(Response)] == null ||
+                    string.IsNullOrEmpty(reader[nameof(Response)].ToString()))
+                {
+                    result.success = false;
+                    return result;
+                }
+                Response = reader[nameof(Response)].ToString();
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+        public override Result validateRow()
+        {
+            Result result = new Result();
+            try
+            {
+                if (this.Tournament_ID == null)
+                {
+                    result.success = false;
+                    result.message = nameof(this.Tournament_ID) + ", of table: " + this.tableName + "-- Cannot be null";
+                }
+                if (this.HostSite_ID == null)
+                {
+                    result.success = false;
+                    result.message = nameof(this.HostSite_ID) + ", of table: " + this.tableName + "-- Cannot be null";
+                }
+                if (this.TournamentHostSite_ID == null)
+                {
+                    result.success = false;
+                    result.message = nameof(this.TournamentHostSite_ID) + ", of table: " + this.tableName + "-- Cannot be null";
+                }
+                if (string.IsNullOrEmpty(this.Request))
+                {
+                    result.success = false;
+                    result.message = nameof(this.Request) + ", of table: " + this.tableName + "-- Cannot be null";
+                }
+                if (string.IsNullOrEmpty(this.Response))
+                {
+                    result.success = false;
+                    result.message = nameof(this.Response) + ", of table: " + this.tableName + "-- Cannot be null";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.success = false;
+                result.message = ex.Message;
+            }
+            return result;
+        }
+
+    }
+
+}
