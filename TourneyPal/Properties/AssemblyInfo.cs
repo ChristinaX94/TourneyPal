e0e4343f526605b9df4127517328047731abbbb3
Christina <christina.trial94@gmail.com>
2024-05-26 19:32:12 +0300

    (v "0.0.0.7") -> Save

--- TourneyPal/Properties/AssemblyInfo.cs
+++ TourneyPal/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 
 
 
-[assembly: AssemblyVersion("0.0.0.6")]
-[assembly: AssemblyFileVersion("0.0.0.6")]
-[assembly: AssemblyInformationalVersion("0.0.0.6")]
+[assembly: AssemblyVersion("0.0.0.7")]
+[assembly: AssemblyFileVersion("0.0.0.7")]
+[assembly: AssemblyInformationalVersion("0.0.0.7")]
