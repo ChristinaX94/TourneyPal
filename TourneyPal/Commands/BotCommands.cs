00a83fd9ac5f49238266cce0a445593bc9544f91
Christina <christina.trial94@gmail.com>
2024-10-11 00:39:36 +0200

    Generalize some calls (Might need to check back on that) Commands: tourneyIn, postAll, postOld, post

--- TourneyPal/Commands/BotCommands.cs
+++ TourneyPal/Commands/BotCommands.cs
@@ -5,8 +5,11 @@ using DSharpPlus.Interactivity.Enums;
 using DSharpPlus.Interactivity.EventHandling;
 using DSharpPlus.Interactivity.Extensions;
 using DSharp​Plus.SlashCommands;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using TourneyPal.Commons;
+using TourneyPal.DataHandling.DataObjects;
 
 namespace TourneyPal
 {
@@ -28,139 +31,113 @@ namespace TourneyPal
             await ctx.CreateResponseAsync("Pong").ConfigureAwait(false);
         }
 
-        [SlashCommand("postOld", "To be deleted")]
-        public async Task PostOld(InteractionContext ctx)
-        {
-            try
-            {
-                List<DiscordEmbed> embeds = new List<DiscordEmbed>();
-                var server = ctx.Guild;
-                var member = ctx.Member;
-
-                if (TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData.Count==0)
-                {
-                    await ctx.CreateResponseAsync("No Data").ConfigureAwait(false);
-                }
-
-                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(member.Mention).AddMention(mention: new UserMention(ctx.User))).ConfigureAwait(false);
-                foreach (var tourney in TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData)
-                {
-                    DiscordEmbed embed = new DiscordEmbedBuilder
-                    {
-                        Title = tourney.Game,
-                        Description = tourney.Name,
-                        Color = DiscordColor.Purple,
-
-                    }
-                    .AddField("Site: ", tourney.HostSite + tourney.URL)
-                    .AddField("Online: ", tourney.Online == null ? " - " : tourney.Online == true ? "Yes" : "No")
-                    .AddField("Location: ", tourney.VenueAddress + ", " + tourney.City + ", "+ tourney.AddrState + ", " + tourney.CountryCode)
-                    .AddField("Date (dd/mm/yyyy): ", tourney.StartsAT == null ? " - " : tourney.StartsAT.Value.Date.ToString("dd/MM/yyyy"))
-                    .AddField("Streams: ", tourney.Streams==null|| tourney.Streams.Count==0 ? " - " : string.Join("\n", tourney.Streams))
-                    .AddField("ID: ", tourney.ID.ToString())
-                    .Build();
-
-                    embeds.Add(embed);
-                }
-
-                var buttons = new PaginationButtons()
-                {
-                    SkipLeft = new(ButtonStyle.Secondary, "leftskip", "1", false),
-                    Left = new(ButtonStyle.Primary, "left", null, false, new(DiscordEmoji.FromName(ctx.Client, ":point_left:"))),
-                    Stop = new(ButtonStyle.Secondary, "stop", ".", true),
-                    Right = new(ButtonStyle.Primary, "right", null, false, new(DiscordEmoji.FromName(ctx.Client, ":point_right:"))),
-                    SkipRight = new(ButtonStyle.Secondary, "rightskip", embeds.Count.ToString(), false)
-                };
-
-                var pages = GeneratePagesInEmbed(embeds);
-                await ctx.Channel.SendPaginatedMessageAsync(user:ctx.Member, 
-                                                            pages: pages, 
-                                                            buttons: buttons, 
-                                                            behaviour: PaginationBehaviour.WrapAround, 
-                                                            deletion: ButtonPaginationBehavior.DeleteButtons).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
-                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-            }
-        }
-
-        private IEnumerable<Page> GeneratePagesInEmbed(List<DiscordEmbed> embeds)
-        {
-            List<Page> list = new List<Page>();
-            try
-            {
-                var count = 1;
-                foreach(var embed in embeds)
-                {
-                    list.Add(new Page("", new DiscordEmbedBuilder(embed).WithFooter($"Tournament {count}/{embeds.Count}")));
-                    count++;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
-                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
-                list = new List<Page>();
-            }
-            return list;
-        }
-
-
-        [SlashCommand("post", "Posts all available Tournaments up to one year")]
+        
+        [SlashCommand("post", "Posts all upcoming Tournaments up to one year")]
         public async Task Post(InteractionContext ctx)
         {
             try
             {
-                var pos = 0;
-                List<DiscordEmbed> embeds = new List<DiscordEmbed>();
-                var server = ctx.Guild;
-                var member = ctx.Member;
+                List<DiscordEmbed> embeds = GetEmbeds(GeneralData.TournamentsData.Where(x => DateOnly.FromDateTime((DateTime)x.StartsAT) >= DateOnly.FromDateTime(DateTime.Now)).OrderBy(x => x.StartsAT).ThenBy(x => x.ID).ToList());
+                await setPages(ctx, embeds).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+            }
+        }
 
-                if (TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData.Count == 0)
+        [SlashCommand("postOld", "Posts all past Tournaments")]
+        public async Task PostOld(InteractionContext ctx)
+        {
+            try
+            {
+                List<DiscordEmbed> embeds = GetEmbeds(GeneralData.TournamentsData.Where(x => DateOnly.FromDateTime((DateTime)x.StartsAT) < DateOnly.FromDateTime(DateTime.Now)).OrderBy(x => x.StartsAT).ThenBy(x => x.ID).ToList());
+                await setPages(ctx, embeds).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+            }
+        }
+
+        [SlashCommand("postAll", "Posts all registered Tournaments in DB")]
+        public async Task PostAll(InteractionContext ctx)
+        {
+            try
+            {
+                List<DiscordEmbed> embeds = GetEmbeds(GeneralData.TournamentsData.OrderBy(x => DateOnly.FromDateTime((DateTime)x.StartsAT)).ThenBy(x => x.ID).ToList());
+
+                var upcomingTournament = GeneralData.TournamentsData.OrderBy(x => DateOnly.FromDateTime((DateTime)x.StartsAT)).ThenBy(x => x.ID).FirstOrDefault(x => DateOnly.FromDateTime((DateTime)x.StartsAT) >= DateOnly.FromDateTime(DateTime.Now));
+                var upcomingTournamentPos = 0;
+                if (upcomingTournament != null)
+                {
+                    upcomingTournamentPos = GeneralData.TournamentsData.IndexOf(upcomingTournament);
+                }
+
+                await setPages(ctx, embeds, upcomingTournamentPos).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+            }
+        }
+
+        [SlashCommand("tourneyIn", "Posts all registered Tournaments in Specific Country")]
+        public async Task AddAsync(InteractionContext ctx, [Option("country", "2 character Country Code")] string country)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(country) || country.ToArray().Length!=2)
+                {
+                    await ctx.CreateResponseAsync("Country must contain 2 characters!").ConfigureAwait(false);
+                }
+                
+                List<DiscordEmbed> embeds = GetEmbeds(GeneralData.TournamentsData.Where(x => x.CountryCode.Equals(country) && DateOnly.FromDateTime((DateTime)x.StartsAT) >= DateOnly.FromDateTime(DateTime.Now)).OrderBy(x => x.StartsAT).ThenBy(x => x.ID).ToList());
+                await setPages(ctx, embeds).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+            }
+        }
+        
+
+        private async Task setPages(InteractionContext ctx, List<DiscordEmbed> embeds, int selectedPos=0)
+        {
+            try
+            {
+                var pos = selectedPos;
+                if (embeds.Count == 0)
                 {
                     await ctx.CreateResponseAsync("No Data").ConfigureAwait(false);
                 }
 
-                
-                foreach (var tourney in TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData)
-                {
-                    DiscordEmbed embed = new DiscordEmbedBuilder
-                    {
-                        Title = tourney.Game,
-                        Description = tourney.Name,
-                        Color = DiscordColor.Purple,
-
-                    }
-                    .AddField("Site: ", tourney.HostSite + tourney.URL)
-                    .AddField("Online: ", tourney.Online == null ? " - " : tourney.Online == true ? "Yes" : "No")
-                    .AddField("Location: ", tourney.VenueAddress + ", " + tourney.City + ", " + tourney.AddrState + ", " + tourney.CountryCode)
-                    .AddField("Date (dd/mm/yyyy): ", tourney.StartsAT == null ? " - " : tourney.StartsAT.Value.Date.ToString("dd/MM/yyyy"))
-                    .AddField("Streams: ", tourney.Streams == null || tourney.Streams.Count == 0 ? " - " : string.Join("\n", tourney.Streams))
-                    .AddField("ID: ", tourney.ID.ToString())
-                    .WithFooter($"Tournament {1+TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData.IndexOf(tourney)}/{TourneyPal.DataHandling.DataObjects.GeneralData.TournamentsData.Count}")
-                    .Build();
-
-                    embeds.Add(embed);
-                }
-
-                var leftButton = new DiscordButtonComponent(ButtonStyle.Secondary, "Lefty", string.Empty, emoji: new(DiscordEmoji.FromName(ctx.Client, ":point_left:")));
-                var goButton = new DiscordLinkButtonComponent(embeds[pos].Fields[0].Value, "Go!");
-                var rightButton = new DiscordButtonComponent(ButtonStyle.Secondary, "Righty", string.Empty, emoji: new(DiscordEmoji.FromName(ctx.Client, ":point_right:")));
-
-                var buttons = new DiscordComponent[] { leftButton, goButton, rightButton };
-
                 await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
-                    .WithContent(member.Mention)
+                    .WithContent(ctx.Member.Mention)
                     .AddMention(mention: new UserMention(ctx.User))
                     .AddEmbed(embeds[pos])
-                    .AddComponents(buttons))
+                    .AddComponents(getButtons(ctx.Client, embeds[pos].Fields[0].Value)))
                     .ConfigureAwait(false);
 
                 ctx.Client.ComponentInteractionCreated += async (s, e) =>
                 {
-                    if (e.Id.Equals("Righty") && pos < embeds.Count-1)
+                    if(e.Message?.Embeds == null)
+                    {
+                        return;
+                    }
+
+                    var selectedEmbed = embeds.FirstOrDefault(x => x.Fields[x.Fields.Count - 1].Value.Equals(e.Message.Embeds.FirstOrDefault().Fields[x.Fields.Count - 1].Value));
+                    pos = embeds.IndexOf(selectedEmbed);
+                    if (pos==-1)
+                    {
+                        return;
+                    }
+
+                    if (e.Id.Equals("Righty") && pos < embeds.Count - 1)
                     {
                         pos++;
                     }
@@ -168,10 +145,12 @@ namespace TourneyPal
                     {
                         pos--;
                     }
-                    buttons[1]= new DiscordLinkButtonComponent(embeds[pos].Fields[0].Value, "Go!");
 
-                    await e.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage,new DiscordInteractionResponseBuilder().AddEmbed(embeds[pos]).AddComponents(buttons));
+                    await e.Interaction.CreateResponseAsync(InteractionResponseType.UpdateMessage, new DiscordInteractionResponseBuilder()
+                        .AddEmbed(embeds[pos])
+                        .AddComponents(getButtons(ctx.Client, embeds[pos].Fields[0].Value)));
                 };
+
             }
             catch (Exception ex)
             {
@@ -180,7 +159,59 @@ namespace TourneyPal
             }
         }
 
+        private List<DiscordComponent> getButtons(DiscordClient client, string url)
+        {
+            try
+            {
+                return new List<DiscordComponent>(){
+                        new DiscordButtonComponent(ButtonStyle.Secondary, "Lefty", string.Empty, emoji: new(DiscordEmoji.FromName(client, ":point_left:"))),
+                        new DiscordLinkButtonComponent(url, "Go!"),
+                        new DiscordButtonComponent(ButtonStyle.Secondary, "Righty", string.Empty, emoji: new(DiscordEmoji.FromName(client, ":point_right:"))) };
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+            }
+            return new List<DiscordComponent>() { };
+        }
 
+        private List<DiscordEmbed> GetEmbeds(List<TournamentData> tourneysSelected)
+        {
+            List<DiscordEmbed> embeds = new List<DiscordEmbed>();
+            try
+            {
+                foreach (var tourney in tourneysSelected)
+                {
+                    DiscordEmbed embed = new DiscordEmbedBuilder
+                    {
+                        Title = tourney.Game,
+                        Description = tourney.Name,
+                        Color = DiscordColor.Purple,
+
+                    }
+                    .AddField("Site: ", tourney.HostSite + tourney.URL)
+                    .AddField("Online: ", tourney.Online == null ? " - " : tourney.Online == true ? "Yes" : "No")
+                    .AddField("Country Code: ", tourney.CountryCode)
+                    .AddField("Location: ", tourney.VenueAddress + ", " + tourney.City + ", " + tourney.AddrState + ", " + tourney.CountryCode)
+                    .AddField("Date (dd/mm/yyyy): ", tourney.StartsAT == null ? " - " : tourney.StartsAT.Value.Date.ToString("dd/MM/yyyy"))
+                    .AddField("Streams: ", tourney.Streams == null || tourney.Streams.Count == 0 ? " - " : string.Join("\n", tourney.Streams))
+                    .AddField("ID: ", tourney.ID.ToString())
+                    .WithFooter($"Tournament {1 + tourneysSelected.IndexOf(tourney)}/{tourneysSelected.Count}")
+                    .Build();
+
+                    embeds.Add(embed);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.log(foundInItem: MethodBase.GetCurrentMethod(),
+                           exceptionMessageItem: ex.Message + " -- " + ex.StackTrace);
+                embeds = new List<DiscordEmbed>();
+            }
+            
+            return embeds;
+        }
     }
 
 }
