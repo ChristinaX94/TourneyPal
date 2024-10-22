8412a5796aab595748a90ca1c629a324388a80b4
Christina <christina.trial94@gmail.com>
2024-10-23 00:07:40 +0200

    2nd try - We have a functional service!

--- TourneyPal.Service/ITourneyPalService.cs
+++ TourneyPal.Service/ITourneyPalService.cs
@@ -1,8 +1,19 @@
-﻿namespace TourneyPal.Service
+﻿using System.Reflection;
+using TourneyPal.Data.Commons;
+
+namespace TourneyPal.Service
 {
     public interface ITourneyPalService
     {
-        public string ping();
-        public void initializeData();
+        public string Ping();
+        public void InitializeData();
+        public void Log(MethodBase? foundInItem, string? messageItem = null, string? exceptionMessageItem = null);
+        public Task RunApiHandler();
+        public Task SearchChallongeEntry(string url);
+        public List<TournamentData> getNewTournaments();
+        public List<TournamentData> getOldTournaments();
+        public List<TournamentData> getAllTournaments();
+        public List<TournamentData> getNewTournamentsByCountryCode(string countryCode);
+        public Task<TournamentData?> getTournamentByURL(string url);
     }
 }
--- TourneyPal.Service/TourneyPalService.cs
+++ TourneyPal.Service/TourneyPalService.cs
@@ -1,22 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using TourneyPal.Commons;
+using TourneyPal.Data.Commons;
+using TourneyPal.DataHandling;
 using TourneyPal.DataHandling.DataObjects;
 
 namespace TourneyPal.Service
 {
     public class TourneyPalService: ITourneyPalService
     {
-        public string ping() 
+        public string Ping() 
         {
-            return "Hello!";
+            return "Pong from service!";
         }
 
-        public void initializeData()
+        public void InitializeData()
         {
             GeneralData.GeneralDataInitialize();
+        }
+
+        public void Log(MethodBase? foundInItem, string? messageItem = null, string? exceptionMessageItem = null)
+        {
+            Logger.log(foundInItem, messageItem, exceptionMessageItem);
+        }
+
+        public async Task RunApiHandler()
+        {
+            await Task.WhenAll(ApiHandler.runAsync());
+        }
+
+        public async Task SearchChallongeEntry(string url)
+        {
+            await ApiHandler.examineSingleChallongeRequest(url);
+        }
+
+        public List<TournamentData> getNewTournaments()
+        {
+            return GeneralData.TournamentsData.Where(x => x.StartsAT >= Common.getDate()).ToList();
+        }
+
+        public List<TournamentData> getOldTournaments()
+        {
+            return GeneralData.TournamentsData.Where(x => x.StartsAT < Common.getDate()).ToList();
+        }
+
+        public List<TournamentData> getAllTournaments()
+        {
+            return GeneralData.TournamentsData.ToList();
+        }
+
+        public List<TournamentData> getNewTournamentsByCountryCode(string countryCode)
+        {
+            return GeneralData.TournamentsData.Where(x => x.CountryCode.Equals(countryCode) && x.StartsAT >= Common.getDate()).ToList();
+        }
+
+        public async Task<TournamentData?> getTournamentByURL(string url)
+        {
+            var tournamentFound = GeneralData.TournamentsData.FirstOrDefault(x => x.URL.Equals(url));
+            if (tournamentFound == null) 
+            {
+                await SearchChallongeEntry(url);
+                tournamentFound = GeneralData.TournamentsData.FirstOrDefault(x => x.URL.Equals(url));
+            }
+
+            return tournamentFound;
         }
     }
 }
