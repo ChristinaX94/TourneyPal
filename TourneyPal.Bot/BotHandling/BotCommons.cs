e0bedb4febf4921e15f00a9a6cd0eff30d68533f
Christina <christina.trial94@gmail.com>
2024-10-23 00:07:40 +0200

    2nd try - We have a functional service!

--- TourneyPal.Bot/BotHandling/BotCommons.cs
+++ TourneyPal.Bot/BotHandling/BotCommons.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TourneyPal.Service;
 
-namespace TourneyPal.Bot.BotHandling
+namespace TourneyPal.BotHandling
 {
     public static class BotCommons
     {
